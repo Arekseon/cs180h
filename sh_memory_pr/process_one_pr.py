40ec9f0ad519688d37f9d753900a1fa4c2763f84
Arekseon <petroys@yandex.ru>
2017-02-08 13:10:36 -0800

    30fps face and vector

--- sh_memory_pr/process_one_pr.py
+++ sh_memory_pr/process_one_pr.py
@@ -1,62 +1,36 @@
-import pygame.camera, pygame.image
 import dlib, cv2, time, utils, mmap, sys, json
-import utils
-import posix_ipc
+import utils, posix_ipc
 from PIL import Image
+import numpy as np
 
-scale_x = 0.2
+scale_x = utils.SCALE_DOWN
 upsample_num = 1
 
-detector = dlib.get_frontal_face_detector()
 predictor_path = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 
+focused_mod = False
+focused_recs = []
+focused_rec_delta = 0.2
 
-
-
-
-temp_img_file_bmp = "photo.bmp"
-temp_img_file_jpeg = "photo_jpg"
+use_Focus = True
 
 def start_camera():
-    pygame.camera.init()
-    cam = pygame.camera.Camera(pygame.camera.list_cameras()[1])
-    cam.start()
-    return cam    
+    cap = cv2.VideoCapture(1)
+    return cap
+   
 
 def get_image_from_camera(cam):
-    return cam.get_image()
+    ret, frame = cam.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray,None,fx=scale_x, fy=scale_x, interpolation = cv2.INTER_CUBIC)
+    width, height = gray.shape[:2]
+    return gray, width, height
 
 def kill_camera(cam):
-    cam.stop()
-    pygame.camera.quit()
+    cam.release()
     
-
-def get_PIL_from_pygame(img):
-    pygame.image.save(img, temp_img_file_bmp)
-    # time.sleep(1)
-
-    # im = Image.open(temp_img_file_bmp)
-    # bg = Image.new("RGB", im.size, (255,255,255,255))
-    # bg.paste(im,im)
-    # bg.save(temp_img_file_jpeg)
-
-    img = Image.open(temp_img_file_bmp)
-    img.save(temp_img_file_jpeg,'jpeg')
-
-    img_grey = cv2.imread(temp_img_file_jpeg, cv2.IMREAD_GRAYSCALE)    
-    img_grey = cv2.resize(img_grey,None,fx=scale_x, fy=scale_x, interpolation = cv2.INTER_CUBIC)
-    width, height = img_grey.shape[:2]
-    return img_grey, width, height
-
-
-def PIL_image_to_pygame(image):
-    mode = image.mode
-    size = image.size
-    data = image.tostring()
-
-    this_image = pygame.image.fromstring(data, size, mode)
-    return this_image
 
 # Check if a point is inside a rectangle
 def rect_contains(rect, point) :
@@ -80,73 +54,100 @@ def sanitize_point(old_point,size):
    return point
 
 def get_faces_points(img):
-    # time_point_0 = time.time()
-    # detector = dlib.get_frontal_face_detector()
-    # predictor_path = "shape_predictor_68_face_landmarks.dat"
-    # predictor = dlib.shape_predictor(predictor_path)
-    # print("time_point_0 in {}".format(time_point_0 - time.time()))
-
-    
+    # cv2.imshow('image2',img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
-    # time_point_1 = time.time()
     detected_faces = detector(img, upsample_num)
-    # print("time_point_1 in {}".format(time_point_1 - time.time()))
-    
-
-    # print("Number of faces detected: {}".format(len(detected_faces)))
+    focused_recs = []
     faces_points = []
-
-
-    # time_point_4 = time.time()
     for k, d in enumerate(detected_faces):
-        # print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-        #    k, d.left(), d.top(), d.right(), d.bottom()))
-
-        # time_point_2 = time.time()
+        # print("Detection : Top: {} Right: {} Bottom: {} Left: {}".format(
+        #      d.top(), d.right(), d.bottom(), d.left()))
+        focused_recs.append({
+            'left':     d.left(),
+            'right':    d.right(),
+            'bottom':   d.bottom(),
+            'top':      d.top()
+            })
         shape = predictor(img, d)
-        # print("time_point_2 in {}".format(time_point_2 - time.time()))
-
-
-        # time_point_3 = time.time()
         points = []
         for i in xrange(0,68):
            point = (shape.part(i).x, shape.part(i).y)
            points.append(sanitize_point(point,img.shape))
         faces_points.append(points)
-        # print("time_point_3 in {}".format(time_point_3 - time.time()))
+    return faces_points, focused_recs
 
-    # print("time_point_4 in {}".format(time_point_4 - time.time()))
+def get_focused_faces_points(img, focused_recs, g_width, g_height):
+    faces_found = []
+    new_focused_recs = []
+    for rec in focused_recs:
+
+        # crop rectangle
+        width = rec['right'] - rec['left']
+        height = rec['top'] - rec['bottom']
+        scaled_rec = {
+            'left':     max(int(rec['left']     - width*focused_rec_delta), 0),
+            'right':    min(int(rec['right']    + width*focused_rec_delta), g_width),
+            'bottom':   min(int(rec['bottom']   - height*focused_rec_delta),g_height),
+            'top':      max(int(rec['top']      + height*focused_rec_delta),0)
+        }
+        offset_x = scaled_rec['left']
+        offset_y = scaled_rec['top']
+
+        croped_img = img[scaled_rec['top']:scaled_rec['bottom'], scaled_rec['left']: scaled_rec['right']]
+
+        # process croped image
+        croped_face_points, new_local_focused_recs = get_faces_points(croped_img)
+    
+        # return global coordinates
+        global_face_points = []
+        for local_face in croped_face_points:
+            global_face = []
+            for local_point in local_face:
+                global_point = (local_point[0] + offset_x, local_point[1]+ offset_y)
+                global_face.append(global_point)
+            global_face_points.append(global_face)
+
+        global_new_focused_recs = []
+        for local_focused_rec in new_local_focused_recs:
+            global_rec = {
+                'left':   local_focused_rec['left']   + offset_x,
+                'right':  local_focused_rec['right']  + offset_x,
+                'bottom': local_focused_rec['bottom'] + offset_y,
+                'top':    local_focused_rec['left']   + offset_y
+            }
+            global_new_focused_recs.append(global_rec)
+        return global_face_points, global_new_focused_recs
+            
 
 
-    return faces_points
 
-def process_image_get_points(cam):
+
+
+def process_image_get_points(cam, focused_recs, focused_mod):
     start_time = time.time()
-    img = get_image_from_camera(cam)
-    img = get_image_from_camera(cam)
+    img, width, height = get_image_from_camera(cam)
+
+    to_console = ("focused mode: {}".format(focused_mod)),
+    if not focused_mod:
+        points, focused_recs = get_faces_points(img)
+    else:
+        points, focused_recs = get_focused_faces_points(img, focused_recs, width, height)
+    # global focused_mod
+    if use_Focus:
+        focused_mod = len(focused_recs)>0
 
 
-    # time_point = time.time()
-    # print("got image from camera in {}".format(time_point - start_time))
-    img, width, height = get_PIL_from_pygame(img)
-    # print("transformed image in {}".format(time.time() - time_point))
-
-
-
-    # time_point = time.time()
-    points =  get_faces_points(img)
-    # print("found points in {}".format(time.time() - time_point))
-    print("frame fps is {}".format(1/(time.time() - start_time))),
+    to_console += ("; frame fps is {}".format(int(1/(time.time() - start_time)))),
     meta = {
         'face_found':len(points)==1,
         'points':points,
         'width':width,
         'height':height
     }
-    # print meta
-    # print("face found: {}".format(len(points)))
-    return meta
+    return meta, focused_recs, focused_mod, to_console
 
 def create_shared_memory_and_semaphore():
     SHARED_MEMORY_NAME = utils.SHARED_MEMORY_NAME#"SHWIFTY_MEMORY"
@@ -174,20 +175,9 @@ def unlink_shared_memort_and_semaphore(mapfile, shared_memory, semaphore):
 def clean_up(SHARED_MEMORY_NAME, SEMAPHORE_NAME):
     try:
         posix_ipc.unlink_shared_memory(SHARED_MEMORY_NAME)
-        s = "memory segment %s removed" % SHARED_MEMORY_NAME
-        print (s)
-    except:
-        print ("memory doesn't need cleanup")
-        
-        
-    try:
         posix_ipc.unlink_semaphore(SEMAPHORE_NAME)
-        s = "semaphore %s removed" % SEMAPHORE_NAME
-        print (s)
     except:
-        print ("semaphore doesn't need cleanup")
-
-
+        print ("memory or semaphore doesn't need cleanup")
     print ("\nAll clean!")
 
 def wait_for_new_message(initial_message, mapfile, semaphore):
@@ -195,8 +185,8 @@ def wait_for_new_message(initial_message, mapfile, semaphore):
     while message_i_just_read == initial_message:
         semaphore.acquire()
         message_i_just_read = utils.read_from_memory(mapfile)
-        if not message_i_just_read == initial_message:
-            utils.write_to_memory(mapfile, utils.JOB_DONE)
+        # if not message_i_just_read == initial_message:
+        #     utils.write_to_memory(mapfile, utils.JOB_DONE)
         semaphore.release()
 
         time.sleep(0.001)
@@ -206,61 +196,73 @@ def wait_for_new_message(initial_message, mapfile, semaphore):
 
 
 if __name__ == '__main__':
+    try:
+        shared_memory, semaphore = create_shared_memory_and_semaphore()
 
-    shared_memory, semaphore = create_shared_memory_and_semaphore()
+        mapfile = mmap.mmap(shared_memory.fd, shared_memory.size)
+        shared_memory.close_fd()
 
-    mapfile = mmap.mmap(shared_memory.fd, shared_memory.size)
-    shared_memory.close_fd()
+        starting_message = utils.FIRST_HANDSHAKE
+        utils.write_to_memory(mapfile, starting_message)
+        message_i_just_read = starting_message
 
-    starting_message = utils.FIRST_HANDSHAKE
-    utils.write_to_memory(mapfile, starting_message)
-    message_i_just_read = starting_message
+        print("Waiting for second process")
+        waiting_time = 0
+        while message_i_just_read == starting_message:
+            sys.stdout.write("\r{0}".format("."*waiting_time))
+            sys.stdout.flush()
+            semaphore.release()
+            time.sleep(1)
+            waiting_time+=1
+            semaphore.acquire()
+            message_i_just_read = utils.read_from_memory(mapfile)
 
-    print("Waiting for second process")
-    waiting_time = 0
-    while message_i_just_read == starting_message:
-    
-        sys.stdout.write("\r{0}".format("."*waiting_time))
-        sys.stdout.flush()
 
-        semaphore.release()
-        
+        print("")
+        print("I feel second process is ready, let's roll")
         time.sleep(1)
-        waiting_time+=1
 
-
-        semaphore.acquire()
-        message_i_just_read = utils.read_from_memory(mapfile)
-
-
-    print("")
-    print("I feel second process is ready, let's roll")
-    time.sleep(1)
-
-    cam = start_camera()
-    meta = process_image_get_points(cam)
-    meta_json = json.dumps(meta)
-    utils.write_to_memory(mapfile, meta_json)
-    semaphore.release()
-
-    start_time = time.time()
-    frame_counter = 0
-    for i in xrange(utils.IMAGE_LOOP_COUNTER):
-        wait_for_new_message(meta_json, mapfile, semaphore)
-        semaphore.acquire()
-        meta = process_image_get_points(cam)
+        cam = start_camera()
+        meta, focused_recs, focused_mod, to_console = process_image_get_points(cam,[], False)
         meta_json = json.dumps(meta)
         utils.write_to_memory(mapfile, meta_json)
+        # utils.write_to_memory(mapfile, utils.THIRD_HANDSHAKE)
         semaphore.release()
-        print("frame_counter: {}".format(frame_counter))
-        frame_counter+=1
+
+        start_time = time.time()
+        frame_counter = 0
+        # focused_mod = False
+        # focused_recs = []
+        # message_i_just_wrote = utils.THIRD_HANDSHAKE
+        while True:
+            # answer = wait_for_new_message(message_i_just_wrote, mapfile, semaphore)
+            answer = wait_for_new_message(meta_json, mapfile, semaphore)
+            if answer == utils.JOB_DONE:
+                break
+            semaphore.acquire()
+            meta, focused_recs, focused_mod, to_console = process_image_get_points(cam, focused_recs, focused_mod)
+            meta_json = json.dumps(meta)
+            utils.write_to_memory(mapfile, meta_json)
+            semaphore.release()
+            to_console = ("{}; frame_counter: {}".format(to_console, frame_counter))
+
+            sys.stdout.write("\r{0}".format(to_console))
+            sys.stdout.flush()
+            message_i_just_wrote = meta_json
+
+            frame_counter+=1
+            # print("_______________________________")
 
 
+        print("")
+        print("average fps: {}".format(frame_counter/(time.time() - start_time)))
+        print("My job here is done")
+        time.sleep(1)
+        
+        kill_camera(cam)
+        print("Unlinking memory and semaphore")
+        unlink_shared_memort_and_semaphore(mapfile, shared_memory, semaphore)
 
-    print("average fps: {}".format(utils.IMAGE_LOOP_COUNTER/(time.time() - start_time)))
-    print("My job here is done")
-    time.sleep(1)
-    
-    kill_camera(cam)
-    print("Unlinking memory and semaphore")
-    unlink_shared_memort_and_semaphore(mapfile, shared_memory, semaphore)
+    finally:
+        kill_camera(cam)
+        # unlink_shared_memort_and_semaphore(mapfile, shared_memory, semaphore)
--- sh_memory_pr/process_two_pr.py
+++ sh_memory_pr/process_two_pr.py
@@ -23,15 +23,19 @@ def wait_for_new_message(initial_message, mapfile, semaphore):
         semaphore.acquire()
         message_i_just_read = utils.read_from_memory(mapfile)
         if not message_i_just_read == initial_message:
-            utils.write_to_memory(mapfile, utils.JOB_DONE)
+            utils.write_to_memory(mapfile, utils.READY_FOR_NEXT)
         semaphore.release()
 
         time.sleep(0.001)
     return message_i_just_read    
 
+def write_last_message(mapfile, semaphore):
+    semaphore.acquire()
+    utils.write_to_memory(mapfile, utils.JOB_DONE)
+    semaphore.release()    
 
 def draw_point(img, p, color ) :
-    cv2.circle (img, p, 2, (0,0,255), -1)
+    cv2.circle (img, p, 1, (0,0,255), -1)
 
 def json_to_obj(str):
     try:
@@ -43,13 +47,68 @@ def json_to_obj(str):
     return meta
 
 def draw_points_on_blank(meta):
-    blank_image = np.zeros((meta['width'], meta['height'], 3), np.uint8)
+    blank_image = np.zeros((int(meta['width']), int(meta['height']), 3), np.uint8)
     for faces in meta['points']: 
         for p in faces:
             draw_point(blank_image, (p[0],p[1]), (0,0,255))
     # cv2.imshow('image',blank_image)
     # cv2.waitKey(1)
     return blank_image
+
+def resize_back(blank_image):
+    return cv2.resize(blank_image,None,fx=(1/utils.SCALE_DOWN), fy=(1/utils.SCALE_DOWN), interpolation = cv2.INTER_CUBIC)
+    
+
+def draw_face_vector_on_blank(meta, blank_image):
+    size = blank_image.shape
+    
+    image_points = np.array([
+                            meta['points'][0][33],# (359, 391),     # Nose tip
+                            meta['points'][0][8],# (399, 561),     # Chin
+                            meta['points'][0][36],# (337, 297),     # Left eye left corner
+                            meta['points'][0][45],# (513, 301),     # Right eye right corne
+                            meta['points'][0][48],# (345, 465),     # Left Mouth corner
+                            meta['points'][0][54]# (453, 469)      # Right mouth corner
+                        ], dtype="double")
+
+
+    model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip
+                            (0.0, -330.0, -65.0),        # Chin
+                            (-225.0, 170.0, -135.0),     # Left eye left corner
+                            (225.0, 170.0, -135.0),      # Right eye right corne
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner
+                            (150.0, -150.0, -125.0)      # Right mouth corner
+                         
+                        ])
+
+
+    focal_length = size[1]
+    center = (size[1]/2, size[0]/2)
+    # focal_length = meta['width']
+    # center = (meta['width']/2, meta['height']/2)
+
+    camera_matrix = np.array(
+                             [[focal_length, 0, center[0]],
+                             [0, focal_length, center[1]],
+                             [0, 0, 1]], dtype = "double"
+                             )
+
+    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs)#, flags=cv2.CV_ITERATIVE)
+
+
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+    # for p in image_points:
+    #     cv2.circle(blank_image, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+
+    p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+    p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+     
+    cv2.line(blank_image, p1, p2, (255,0,0), 2)
+    return blank_image
+
+
 
 if __name__ == '__main__':
     shared_memory, semaphore = access_shared_memory_and_semaphore()
@@ -66,19 +125,28 @@ if __name__ == '__main__':
     utils.write_to_memory(mapfile, utils.SECOND_HANDSHAKE)
     semaphore.release()
     message_i_just_read = utils.SECOND_HANDSHAKE
+    # message_i_just_read = wait_for_new_message(message_i_just_read, mapfile, semaphore, False)
+        
 
 
-    for i in xrange(utils.IMAGE_LOOP_COUNTER+1):
+    while True:
         message_i_just_read = wait_for_new_message(message_i_just_read, mapfile, semaphore)
-        meta = json_to_obj(message_i_just_read)
-        blank_image = draw_points_on_blank(meta)
-
-        cv2.imshow('image',blank_image)
-        if i == utils.IMAGE_LOOP_COUNTER:
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        if message_i_just_read == utils.THIRD_HANDSHAKE or message_i_just_read== utils.SECOND_HANDSHAKE:
+            continue
         else:
-            cv2.waitKey(1)
-        message_i_just_read = utils.JOB_DONE
+            meta = json_to_obj(message_i_just_read)
+            blank_image = draw_points_on_blank(meta)
+            if len(meta['points'])>0:
+                blank_image = draw_face_vector_on_blank(meta, blank_image)
+
+            blank_image = resize_back(blank_image)
+
+            cv2.imshow('image',blank_image)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                write_last_message(mapfile, semaphore)
+                break
+            else:
+                message_i_just_read = utils.READY_FOR_NEXT
 
 
